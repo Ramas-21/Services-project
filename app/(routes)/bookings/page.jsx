32795bf7c4fcf5d0a1f08485b56026bb3e14619b
Lawrence <lawrencehome21@gmail.com>
2024-06-29 17:27:47 +0300

    installed a package for formatting dates (momentjs)

--- app/(routes)/bookings/page.jsx
+++ app/(routes)/bookings/page.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+'use client';
+import React, {useEffect, useState} from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import BookingList from "./_components/BookingList";
+import Api from "@/app/_services/Api";
+import { useSession } from 'next-auth/react'
 
 const userBookings = () => {
+    const {data} = useSession()
+    const [userBookings, setUserBookings] = useState([]);
+
+    useEffect(() => {
+        data&&GetUserBookings();
+    },[data]);
+
+    const GetUserBookings = () => {
+        Api.GetUserBookings(data.user.email)
+        .then(resp=>{
+            console.log(resp)
+            setUserBookings(resp.bookings);
+        })
+    }
+
+
   return (
     <div className="my-10 mx-5 md:mx-36">
         <h2 className="font-bold text-[18px] my-2">My Booked Services</h2>
